ec24640f67cdc9533df4dca15a35d7f00ba48392
LeeWest89 <lee.west@atlasschool.com>
2025-08-23 19:22:44 -0500

    setting up components

--- README.md
+++
@@ -1,5 +0,0 @@
-# The Joy of Painting
-
-## Overview
-
-Adding a front-end and database
---
+++ joy_of_painting/src/components/navBar.tsx
@@ -0,0 +1,7 @@
+import searchBar from "./searchBar";
+import ddDates from "./ddDates";
+import ddColors from "./ddColors";
+import ddSeason from "./ddSeason";
+import ddSubjest from "./ddSubject";
+
+// 
